bc7112f3c8fbab3bdf254c4044342f4bc5398f3c
Ryan Hild <rhild@starbucks.com>
2017-06-16 13:08:39 -0700

    [add] initial install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -euf -o pipefail
+
+mkdir -p ~/.vim/bundle
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+for i in .atom .gitconfig .vimrc .zshrc; do
+  ln -s ~/dotfiles/$i ~/$i
+done
+
+apm install --packages-file ~/.atom/packages.list
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+for i in $(cat brew.list); do
+  brew install $i
+done
